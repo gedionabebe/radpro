e50921e9d93ae8f7f4ff821d58c86af3ac1a5e73
Gedion <gedionandme@gmail.com>
2019-12-31 10:36:50 +0300

    index

--- index.php
+++ index.php
@@ -1,29 +1,49 @@
 <?php
 
 get_header();
-
-
-if(have_posts()):
-	while (have_posts()) :
-     the_post(); 
-     //title 
-     
-     ?>
-     <h1><a href="<?php the_permalink();?>">
-     <?php the_title( );?>
-     </a></h1>
-     
-     
-<?php
-     the_excerpt();
-
-	endwhile;
-
-endif;
-
-//add footer 
-get_footer();
-
-
-
 ?>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-8">
+
+      <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+
+        <?php
+        if ( have_posts() ) :
+
+          if ( is_home() && ! is_front_page() ) :
+            ?>
+            <header>
+              <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+            </header>
+            <?php
+          endif;
+
+         
+          while ( have_posts() ) :
+            the_post();
+
+            
+            get_template_part( 'template-parts/content', get_post_type() );
+
+          endwhile;
+
+          the_posts_navigation();
+
+        else :
+
+          get_template_part( 'template-parts/content', 'none' );
+
+        endif;
+        ?>
+
+        </main><!-- #main -->
+      </div><!-- #primary -->
+    </div>
+    <div class="col-sm-4">
+    <?php get_sidebar(); ?>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
