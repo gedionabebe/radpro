4085773ad9e96b2900c069cab18438f99cd18505
Gedion <gedionandme@gmail.com>
2020-01-30 10:04:03 +0300

    footer edit

--- Footer.php
+++ Footer.php
@@ -48,16 +48,10 @@
 		
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'gedion' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'gedion' ), 'WordPress' );
-				?>
+				
 			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'gedion' ), 'gedion', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+			
+			<p>Poudly Powered by Gedion </p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
