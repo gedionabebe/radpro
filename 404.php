a80a43033027518b936ac7fc64eba217e226f2d6
Gedion <gedionandme@gmail.com>
2020-02-02 23:20:16 +0300

    404 edit

--- 404.php
+++ 404.php
@@ -22,8 +22,9 @@ get_header();
 
 				<div class="page-content">
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a different page.', 'gedion' ); ?></p>
-
 					
+
+
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
